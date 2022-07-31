83fb5671c5854fc03d65fb7cfa0f46d83df8de94
Мамонтов Виктор Викторович <VViMamontov@sberbank.ru>
2022-07-31 11:37:57 +0300

    init

---
+++ src/main/kotlin/ru/viktorxdev/easypayment/EasyPaymentApplication.kt
@@ -0,0 +1,11 @@
+package ru.viktorxdev.easypayment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyPaymentApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasyPaymentApplication>(*args)
+}
